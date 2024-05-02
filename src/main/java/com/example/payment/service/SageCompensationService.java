7f0b364c7a6e9646b4aa10cfcdca5c1f23b7b3df
Aleksey Inshakov <aleksey.inshakov@domrf.ru>
2024-05-02 04:07:59 +0300

    Add DB and Kafka K8s configuration

--- src/main/java/com/example/payment/service/SageCompensationService.java
+++ src/main/java/com/example/payment/service/SageCompensationService.java
@@ -6,10 +6,11 @@ import com.example.payment.dto.WarehouseReservationRejectedMessage;
 import com.example.payment.entity.ProductReservationEntity;
 import com.example.payment.entity.ProductReservationStatus;
 import com.example.payment.repository.ProductReservedRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 @RequiredArgsConstructor
